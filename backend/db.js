e91b21847667847770fca6d40426110312ccfc09
RajsekharPanda <rajsekharpandawork@gmail.com>
2024-01-09 14:11:57 +0530

    backend-checks-done

--- backend/db.js
+++ backend/db.js
@@ -1,11 +1,11 @@
 const mongoose = require("mongoose");
 const { boolean } = require("zod");
-mongoose.connect("mongodb://localhost:27017");
+mongoose.connect("mongodb://localhost:27017/todo");
 
 const todoSchema = new mongoose.Schema({
   title: String,
   description: String,
-  completed: Boolean
+  completed: Boolean,
 });
 
 const todo = mongoose.model("todos", todoSchema);
